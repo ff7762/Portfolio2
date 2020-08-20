dee15dc4f8d35cb8d9fc2c27912f089a7c0cf23b
ff7762 <67229067+ff7762@users.noreply.github.com>
2020-08-20 08:03:20 +0100

    Added Js, and work and contact sections

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("test");
+
+$.fn.followTo = function (pos) {
+    var $this = this,
+        $window = $(window);
+
+    $window.scroll(function (e) {
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'fixed',
+                top: 0
+            });
+        }
+    });
+};
+
+$('#topbar').followTo(350);
